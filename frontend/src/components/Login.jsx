2da368c883a365d330edad1278bf36b721b00300
ColeShirakata <coleshirakata@gmail.com>
2023-09-07 19:28:26 -0700

    Initial setup with vite

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,10 @@
+const Login = () => {
+    return (
+        <div>
+            <h1>Spotify Dashboard</h1>
+            <button>Login with Spotify</button>
+        </div>
+    )
+}
+
+export default Login
