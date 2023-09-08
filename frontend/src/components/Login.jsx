53d7d3e5aefc981dfb8c122deca4264ac3ad3dea
ColeShirakata <coleshirakata@gmail.com>
2023-09-07 22:34:51 -0700

    Adds user authentication

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,15 @@
+import axios from 'axios'
+
 const Login = () => {
+    const handleLogin = async () => {
+        const response = await axios.get('http://localhost:3000/login')
+        window.location.href = response.data
+    }
+
     return (
         <div>
             <h1>Spotify Dashboard</h1>
-            <button>Login with Spotify</button>
+            <button onClick={handleLogin}>Login with Spotify</button>
         </div>
     )
 }
